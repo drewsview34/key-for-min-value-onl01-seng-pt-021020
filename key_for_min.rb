8e502c60ba02be889dd7ff89026abbec616e37fe
Andrew Hinojosa <drews_view@hotmail.com>
2020-02-23 06:34:11 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -10,3 +10,15 @@ def key_for_min_value(name_hash)
       
       return key
 end
+
+def key_for_min_value(hash)
+  lowest_key = nil
+  lowest_value = Float::INFINITY
+  hash.each do |k, v|
+    if v < lowest_value
+      lowest_value = v
+      lowest_key = k
+    end
+  end
+  lowest_key
+end
